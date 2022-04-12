a3762576857116bf3d858c5b7cabd28ed25181a5
Serdar Sayın <nefer.kha.ptah@gmail.com>
2022-04-12 03:20:21 +0300

    always_comb block -> assignments

--- src/inst_decoder.sv
+++ src/inst_decoder.sv
@@ -8,32 +8,35 @@ module inst_decoder (
     output logic [15 : 0] w
 );
 
-
-  always_comb begin
-    case (inst.kind)
-      1'b0: begin
-        w   = inst;
-        dst = 3'b100;
-        opc = 6'b000000;
-        j   = 3'b000;
-        ci  = 1'b0;
-        sm  = 1'b0;
-
-      end
-      1'b1: begin
-        ci  = 1'b1;
-        w   = 0;
-        sm  = inst.sm;
-        opc = inst.opc;
-        dst = inst.dst;
-        j   = inst.j;
-
-      end
-      default: begin
-
-      end
-    endcase
-  end
-
+  assign ci = inst.kind;
+  assign w[15] = ~inst.kind & inst[15];
+  assign w[14] = ~inst.kind & inst[14];
+  assign w[13] = ~inst.kind & inst[13];
+  assign w[12] = ~inst.kind & inst[12];
+  assign w[11] = ~inst.kind & inst[11];
+  assign w[10] = ~inst.kind & inst[10];
+  assign w[9] = ~inst.kind & inst[9];
+  assign w[8] = ~inst.kind & inst[8];
+  assign w[7] = ~inst.kind & inst[7];
+  assign w[6] = ~inst.kind & inst[6];
+  assign w[5] = ~inst.kind & inst[5];
+  assign w[4] = ~inst.kind & inst[4];
+  assign w[3] = ~inst.kind & inst[3];
+  assign w[2] = ~inst.kind & inst[2];
+  assign w[1] = ~inst.kind & inst[1];
+  assign w[0] = ~inst.kind & inst[0];
+  assign sm = inst.sm & inst.kind;
+  assign opc[5] = inst.opc[5] & inst.kind;
+  assign opc[4] = inst.opc[4] & inst.kind;
+  assign opc[3] = inst.opc[3] & inst.kind;
+  assign opc[2] = inst.opc[2] & inst.kind;
+  assign opc[1] = inst.opc[1] & inst.kind;
+  assign opc[0] = inst.opc[0] & inst.kind;
+  assign dst[2] = ~inst.kind | inst.dst[2];
+  assign dst[1] = inst.kind & inst.dst[1];
+  assign dst[0] = inst.kind & inst.dst[0];
+  assign j[2] = inst.j[2] & inst.kind;
+  assign j[1] = inst.j[1] & inst.kind;
+  assign j[0] = inst.j[0] & inst.kind;
 
 endmodule
