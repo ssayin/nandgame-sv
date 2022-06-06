84bb30a977b34b3f4d9c1c4fc2213ab278173cd5
Serdar Sayın <nefer.kha.ptah@gmail.com>
2022-06-06 18:40:04 +0300

    fix vl_fatal cause mem leak

--- sim/interactive.cpp
+++ sim/interactive.cpp
@@ -15,40 +15,63 @@ struct handle {
   PLI_INT32 format;
 };
 
-std::vector<handle> handles{{"Computer.computer.cont0.D", vpiIntVal},
-                            {"Computer.computer.cont0.A_star", vpiIntVal}};
+class VPIObject {
+public:
+  VPIObject(const std::string &name);
+  VPIObject(VPIObject &p, std::size_t index);
+  ~VPIObject();
+
+  int readInteger();
+  void putInteger(int val);
+  std::string getStr();
+
+private:
+  vpiHandle h;
+  s_vpi_value v;
+};
+
+VPIObject::VPIObject(const std::string &name) {
+  h = vpi_handle_by_name((PLI_BYTE8 *)name.c_str(), NULL);
+  if (!h)
+    throw std::runtime_error("");
+}
+
+VPIObject::VPIObject(VPIObject &p, std::size_t index) {
+  h = vpi_handle_by_index(p.h, index);
+  if (!h)
+    throw std::runtime_error("");
+}
+
+VPIObject::~VPIObject() { vpi_free_object(h); }
+
+int VPIObject::readInteger() {
+  v.format = vpiIntVal;
+  vpi_get_value(h, &v);
+  return v.value.integer;
+}
+
+void VPIObject::putInteger(int val) {
+  v.format = vpiIntVal;
+  v.value.integer = val;
+  vpi_put_value(h, &v, NULL, vpiNoDelay);
+}
+
+std::string VPIObject::getStr() { return vpi_get_str(vpiName, h); }
 
 void init_instruction_rom(std::deque<uint16_t> &insts) {
-  vpiHandle h;
-  h = vpi_handle_by_name((PLI_BYTE8 *)"Computer.computer.rom0.mem", NULL);
-  s_vpi_value v;
-  if (!h)
-    vl_fatal(__FILE__, __LINE__, "interactive", "No handle found");
-
+  VPIObject rom("Computer.computer.rom0.mem");
   std::size_t end = insts.size();
   for (std::size_t i = 0; i < end; i++) {
-    vpiHandle index = vpi_handle_by_index(h, i);
-    v.format = vpiIntVal;
-    v.value.integer = insts[i];
-    vpi_put_value(index, &v, NULL, vpiNoDelay);
-    vpi_free_object(index);
+    VPIObject(rom, i).putInteger(insts[i]);
   }
-  vpi_free_object(h);
 }
 
 void read_handles_vpi() {
-  vpiHandle h;
-  for (const handle &hand : handles) {
-    h = vpi_handle_by_name((PLI_BYTE8 *)hand.str.c_str(), NULL);
-    s_vpi_value v;
-    if (!h)
-      vl_fatal(__FILE__, __LINE__, "interactive", "No handle found");
+  VPIObject regD("Computer.computer.cont0.D");
+  VPIObject regMem("Computer.computer.cont0.A_star");
 
-    v.format = hand.format;
-    vpi_get_value(h, &v);
-    std::cout << vpi_get_str(vpiName, h) << ": " << v.value.integer << ", ";
-  }
-  std::cout << std::endl;
+  std::cout << regD.getStr() << ": " << regD.readInteger() << ", "
+            << regMem.getStr() << ": " << regMem.readInteger() << std::endl;
 }
 
 int main(int argc, char **argv, char **env) {
@@ -79,14 +102,16 @@ int main(int argc, char **argv, char **env) {
   int res = drv();
   if (!res) {
     init_instruction_rom(drv.insts);
-    int cycle_count;
+    int cycle_count = drv.insts.size();
     if (argc == 3 && !strcmp(argv[1], "-c")) {
       cycle_count = std::atoi(argv[2]);
       // cycle_count will be assigned 0, if an error occurs
       // zero cycles will not mean anything anyways
       if (cycle_count == 0)
         return 0;
-    }
+    } else
+      std::cout << "Cycle count was not set. Using instruction count."
+                << std::endl;
 
     int end = (cycle_count << 1);
     for (std::size_t i = 0; i < end; i++) {
